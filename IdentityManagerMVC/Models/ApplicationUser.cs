039e4dac161c6abf22101caaf97bac42063401f1
alper yavuz ün <yavuzun50@hotmail.com>
2023-02-09 21:18:51 +0300

    Add project files.

---
+++ IdentityManagerMVC/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models
+{
+    // we are extending (adding new props) our identity table
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ IdentityManagerMVC/Views/Shared/_LoginPartialCustom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityManager.Pages.Shared
+{
+    public class _LoginPartialCustomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
